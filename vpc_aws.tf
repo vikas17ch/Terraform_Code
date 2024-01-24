4a74d68569ec2bb880cca3e4d78ea7fb8ad88e40
vikas chava <87170539+vikas17ch@users.noreply.github.com>
2024-01-24 09:12:50 -0500

    Update vpc_aws.tf

--- vpc_aws.tf
+++ vpc_aws.tf
@@ -2,52 +2,52 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_vpc" "Anil_vpc" {
+resource "aws_vpc" "vikas_vpc" {
   cidr_block            = "10.0.0.0/16"
   enable_dns_support    = true
   enable_dns_hostnames  = true
 
   tags = {
-    Name = "AnilVPC"
+    Name = "vikasVPC"
   }
 }
 
-resource "aws_internet_gateway" "Anil_igw" {
-  vpc_id = aws_vpc.Anil_vpc.id
+resource "aws_internet_gateway" "vikas_igw" {
+  vpc_id = aws_vpc.vikas_vpc.id
 
   tags = {
-    Name = "AnilIGW"
+    Name = "vikasIGW"
   }
 }
 
-resource "aws_route_table" "Anil_public_route" {
-  vpc_id = aws_vpc.Anil_vpc.id
+resource "aws_route_table" "vikas_public_route" {
+  vpc_id = aws_vpc.vikas_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.Anil_igw.id
+    gateway_id = aws_internet_gateway.vikas_igw.id
   }
 
   tags = {
-    Name = "AnilPublicRouteTable"
+    Name = "vikasPublicRouteTable"
   }
 }
 
-resource "aws_subnet" "Anil_public_subnet" {
-  vpc_id                  = aws_vpc.Anil_vpc.id
+resource "aws_subnet" "vikas_public_subnet" {
+  vpc_id                  = aws_vpc.vikas_vpc.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-2a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "AnilPublicSubnet"
+    Name = "vikasPublicSubnet"
   }
 }
 
 resource "aws_security_group" "my_security_group" {
-  name        = "AnilSecurityGroup"
+  name        = "vikasSecurityGroup"
   description = "Allow SSH traffic from my public IP"
-  vpc_id      = aws_vpc.Anil_vpc.id
+  vpc_id      = aws_vpc.vikas_vpc.id
 
   ingress {
     from_port   = 22
@@ -64,20 +64,20 @@ resource "aws_security_group" "my_security_group" {
   }
 }
 
-resource "aws_instance" "Anil_ec2_instance" {
+resource "aws_instance" "vikas_ec2_instance" {
   ami                          = "ami-0cd3c7f72edd5b06d"
   instance_type                = "t2.micro"
-  key_name                     = "Kumar"
+  key_name                     = "splunk"
   vpc_security_group_ids       = [aws_security_group.my_security_group.id]
-  subnet_id                    = aws_subnet.Anil_public_subnet.id
+  subnet_id                    = aws_subnet.vikas_public_subnet.id
   associate_public_ip_address  = true
 
   tags = {
-    Name = "AnilEC2Instance"
+    Name = "vikasEC2Instance"
   }
 }
 
-resource "aws_route_table_association" "Anil_subnet_association" {
-  subnet_id      = aws_subnet.Anil_public_subnet.id
-  route_table_id = aws_route_table.Anil_public_route.id
+resource "aws_route_table_association" "vikas_subnet_association" {
+  subnet_id      = aws_subnet.vikas_public_subnet.id
+  route_table_id = aws_route_table.vikas_public_route.id
 }
