a476b7fc7baa03ebef4c4ab8887b891946a2fbdd
vikas chava <87170539+vikas17ch@users.noreply.github.com>
2024-01-24 17:12:36 -0500

    Update vpc_aws.tf
    updated subnet availability zone

--- vpc_aws.tf
+++ vpc_aws.tf
@@ -36,7 +36,7 @@ resource "aws_route_table" "vikas_public_route" {
 resource "aws_subnet" "vikas_public_subnet" {
   vpc_id                  = aws_vpc.vikas_vpc.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-east-2a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
